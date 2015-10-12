dbef2092e789402c501bead89f2dbb8153616937
Gnirt <philippetring@gmail.com>
2015-10-12 16:45:10 +0200

    Add multiple filters

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ Gnirt.Main = (function() {
     soundSource,
     soundBuffer,
     url = 'http://thingsinjars.com/lab/web-audio-tutorial/hello.mp3';
+  var idx = 0;
+  var filters = ['grayscale', 'sepia', 'blur', 'brightness',
+               'contrast', 'hue-rotate', 'hue-rotate2',
+               'hue-rotate3', 'saturate', 'invert', '', 'drop-shadow'];
 
   function setup() {
     scene = new THREE.Scene();
@@ -143,6 +147,14 @@ Gnirt.Main = (function() {
     mesh.rotation.y = Gnirt.Utils.degToRad(Gnirt.Utils.randomRange(45, 135));
 
     scene.add(mesh);
+  }
+
+  function changeFilter(el) {
+    el.className = '';
+    var effect = filters[idx++ % filters.length]; // loop through filters.
+    if (effect) {
+      el.classList.add(effect);
+    }
   }
 
   function addLighting() {
